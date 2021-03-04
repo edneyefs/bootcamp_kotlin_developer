5ea3925763f323a2f1f31c1fb19e27701baffdeb
Edney Ferreira <edneysilva20@hotmail.com>
2021-03-03 21:17:00 -0300

    Estruturas de controle em Kotlin

---
+++ 01_INTRODUÇÃO_AO_KOTLIN/12_ESTRUTURAS_DE_CONTROLE.kts
@@ -0,0 +1,19 @@
+if(expressão){
+    //bloco de código
+}else if (expressão){
+    //bloco de código
+}else{
+    //bloco de código
+}
+
+
+when {
+    case1 -> {}
+    case2 -> {}
+    case3 -> {}
+    else -> {}
+}
+
+
+val a:Int? = null
+var number = a ?: 0
