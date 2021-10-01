2cba3a19262924b5532aa4d4fd68a16b7277f6a3
edneyefs <edneysilva20@hotmail.com>
2021-09-30 23:32:15 -0300

    Importação e criação de um relógio.

--- src/br/com/cv/PrimeiroPrograma.java
+++ src/br/com/cv/PrimeiroPrograma.java
@@ -1,8 +1,14 @@
 package br.com.cv;
 
+import java.util.Date;
+
 public class PrimeiroPrograma {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é ");
+        System.out.println(relogio.toString());
     }
 }
